e2216f6ce0b5ff109037d22b8e8243bf8f940dcc
zml <2786885366@qq.com>
2017-12-02 17:56:09 +0800

    修复角色功能修改的BUG

--- src/main/java/cn/yuntangnet/duizhang/modules/system/service/impl/SystemRoleMenuServiceImpl.java
+++ src/main/java/cn/yuntangnet/duizhang/modules/system/service/impl/SystemRoleMenuServiceImpl.java
@@ -3,6 +3,7 @@ package cn.yuntangnet.duizhang.modules.system.service.impl;
 import cn.yuntangnet.duizhang.modules.system.entity.SystemRoleMenu;
 import cn.yuntangnet.duizhang.modules.system.mapper.SystemRoleMenuMapper;
 import cn.yuntangnet.duizhang.modules.system.service.ISystemRoleMenuService;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,7 +34,10 @@ public class SystemRoleMenuServiceImpl extends ServiceImpl<SystemRoleMenuMapper,
     @Transactional
     public void saveOrUpdate(Long roleId, List<Long> menuIdList) {
         //先删除角色与菜单关系
-        deleteById(roleId);
+        //deleteById(roleId);
+        EntityWrapper<SystemRoleMenu> wrapper = new EntityWrapper<>();
+        wrapper.eq("role_id", roleId);
+        delete(wrapper);
 
         if (menuIdList.size() == 0) {
             return;
